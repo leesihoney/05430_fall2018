64dd3c9d53fbaf5e703a39a4eb4d316f5a9c573f
= <siheonl@andrew.cmu.edu>
2018-10-29 22:48:52 -0400

    reinitializing

--- Labs/week8_lab/PUI-Lab8-master/main.js
+++ Labs/week8_lab/PUI-Lab8-master/main.js
@@ -1,36 +1,130 @@
 
-    var picts= ["cat_one.jpg", "cat_two.jpg", "cat_three.jpg", "cat_four.jpg", "cat_five.jpeg"]
-    var names = ["Mason", "Belle", "ChiChi", "Nabi", "Kokoro"]
-    var ages = [2, 1, 5, 3, 4]
+function Cat(name, age){
+    this.name = name;
+    this.age = age;
+    this.type = "Cat";
+    this.image = "cat_one.jpg";
+};
+
+function Tiger(name, age){
+    this.name = name;
+    this.age = age;
+    this.type = "Tiger";
+    this.image = "tiger.jpg";
+};
+
+function Penguin(name, age){
+    this.name= name;
+    this.age = age;
+    this.type = "Penguin";
+    this.image = "penguin.jpg";
+}
+
+var animals = [new Cat(), new Tiger(), new Penguin()];
+var names = ["Tom", "Titi", "Choo", "Nancy"];
+
+function generateRandomIndex(maxIndex){
+    return Math.floor(Math.random()*maxIndex)
+};
+
+function generateRandomName(){
+    var randomIndex = generateRandomIndex(names.length);
+    return names[randomIndex];
+};
+
+function generateRandomAge(){
+    var randomAge = generateRandomIndex(15);
+    return randomAge;
+}
+
+function generateRandomAnimal(){
+    var randomIndex = generateRandomIndex(animals.length);
+    var randomAnimal = animals[randomIndex];
+    if(randomAnimal instanceof Cat)
+    {
+        return new Cat(generateRandomName(), generateRandomAge());
+    }
+    else if(randomAnimal instanceof Tiger)
+    {
+        return new Tiger(generateRandomName(), generateRandomAge());
+    }
+    else if(randomAnimal instanceof Penguin)
+    {
+        return new Penguin(generateRandomName(), generateRandomAge());
+    }
+}
 
 
+// jQuery put at the bottom
+$(document).ready(()=> {
 
-    class Cat {
-        constructor(pic, name, age)
+    var animals = JSON.parse(localStorage.getItem("savedAnimals"));
+    var maxAnimalsLength = 5;
+    if(animals===null)
+    {
+        for(var i; i<maxAnimalsLength; i++){
+            animals = [];
+            animals.push(generateRandomAnimal())
+        };
+    };
+
+    else if(animals.constructor===Array)
+    {
         {
-            this.pict = pic
-            this.name = name
-            this.age= age
+            for(var i; i<maxAnimalsLength-animals.length; i++){
+                animals.push(generateRandomAnimal())
+        };
+
+    };
+
+    for(var i; i<animals.length;i++)
+    {
+        var animal = animals[i]
+        $('#container').append(
+            "<div class='animal'>" +
+            "<h1 class='animal-name'>" +animal.name "</h1>" +
+            "<img class='animal-img' src=" + animal.image +">" +
+            "<h3 class='animal-age'>" + animal.age + "</h3>" +
+            "<button class = 'save-button' type='button'>" + statusButtonText(animal) +"</button>" +
+            "<p class = 'exp'>" + paragraphText(animal) + "</p>"
+            "</div>"
+        )
+    };
+
+    if($('.save-button').click(function(){
+        var targetAnimal = findtargetAnimal($(this));
+        if(hasAnimal(targetAnimal))
+        {
+            var animalIndex = findIndex(targetAnimal);
+            var result = JSON.parse(localStorage.getItem("savedAnimals")).splice(animalIndex, 1);
+            localStorage.setItem("savedAnimals", JSON.stringify(result));
+        }
+        else{
+            var result = JSON.parse(localStorage.getItem("savedAnimals"));
+            result.push(targetAnimal);
+        }
+            localStorage.setItem("savedAnimals", JSON.stringify(result));
+    }));
+
+function hasAnimal(animal){
+    var storageAnimals = JSON.parse(localStorage.getItem("savedAnimals"));
+    if(storageAnimals===null)
+    {
+        return false;
+    }
+    else
+    {
+        for(var i;i<storageAnimals.length;i++)
+        {
+            if(JSON.stringify(storageAnimals[i])===JSON.stringify(animal)){
+                return true;
+            }
         }
     }
+    return false;
+}
 
-    function random(length)
-    {
-        return Math.floor(Math.random() * length);
-    }
-
-
-    // jQuery put at the bottom
-    $(document).ready(()=> {
-        console.log("ready")
-
-        var cat = new Cat(picts[random(picts.length)], names[random(names.length)], ages[random(ages.length)])
-        console.log(cat)
-        console.log(cat.pict)
-        console.log(cat.name)
-        console.log(cat.age)
-
-        $("#animal-img").attr("src", cat.pict)
-        $("#animal-name").text(cat.name)
-        $("#animal-age").text(cat.age + " years old")
-    })
+function findTargetAnimal($this){
+    $animal = $this.parent();
+    if($animal.)
+}
