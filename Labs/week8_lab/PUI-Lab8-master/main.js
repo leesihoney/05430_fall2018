59eff107481a137ac8233e066fe2dff15cf60d42
= <siheonl@andrew.cmu.edu>
2018-10-30 14:27:14 -0400

    lab8

--- Labs/week8_lab/PUI-Lab8-master/main.js
+++ Labs/week8_lab/PUI-Lab8-master/main.js
@@ -18,7 +18,7 @@ function Penguin(name, age){
     this.age = age;
     this.type = "Penguin";
     this.image = "penguin.jpg";
-}
+};
 
 var animals = [new Cat(), new Tiger(), new Penguin()];
 var names = ["Tom", "Titi", "Choo", "Nancy"];
@@ -35,7 +35,7 @@ function generateRandomName(){
 function generateRandomAge(){
     var randomAge = generateRandomIndex(15);
     return randomAge;
-}
+};
 
 function generateRandomAnimal(){
     var randomIndex = generateRandomIndex(animals.length);
@@ -52,78 +52,47 @@ function generateRandomAnimal(){
     {
         return new Penguin(generateRandomName(), generateRandomAge());
     }
-}
+};
 
 
 // jQuery put at the bottom
 $(document).ready(()=> {
 
-    var animals = JSON.parse(localStorage.getItem("savedAnimals"));
-    var maxAnimalsLength = 5;
-    if(animals===null)
+    var animal = JSON.parse(localStorage.getItem("savedAnimal"));
+    var isSaved = false;
+    if(animal===null)
     {
-        for(var i; i<maxAnimalsLength; i++){
-            animals = [];
-            animals.push(generateRandomAnimal())
-        };
-    };
-
-    else if(animals.constructor===Array)
-    {
-        {
-            for(var i; i<maxAnimalsLength-animals.length; i++){
-                animals.push(generateRandomAnimal())
-        };
-
-    };
-
-    for(var i; i<animals.length;i++)
-    {
-        var animal = animals[i]
-        $('#container').append(
-            "<div class='animal'>" +
-            "<h1 class='animal-name'>" +animal.name "</h1>" +
-            "<img class='animal-img' src=" + animal.image +">" +
-            "<h3 class='animal-age'>" + animal.age + "</h3>" +
-            "<button class = 'save-button' type='button'>" + statusButtonText(animal) +"</button>" +
-            "<p class = 'exp'>" + paragraphText(animal) + "</p>"
-            "</div>"
-        )
-    };
-
-    if($('.save-button').click(function(){
-        var targetAnimal = findtargetAnimal($(this));
-        if(hasAnimal(targetAnimal))
-        {
-            var animalIndex = findIndex(targetAnimal);
-            var result = JSON.parse(localStorage.getItem("savedAnimals")).splice(animalIndex, 1);
-            localStorage.setItem("savedAnimals", JSON.stringify(result));
-        }
-        else{
-            var result = JSON.parse(localStorage.getItem("savedAnimals"));
-            result.push(targetAnimal);
-        }
-            localStorage.setItem("savedAnimals", JSON.stringify(result));
-    }));
-
-function hasAnimal(animal){
-    var storageAnimals = JSON.parse(localStorage.getItem("savedAnimals"));
-    if(storageAnimals===null)
-    {
-        return false;
+        $("#save-button").text("Save");
+        animal = generateRandomAnimal();
     }
+
     else
     {
-        for(var i;i<storageAnimals.length;i++)
-        {
-            if(JSON.stringify(storageAnimals[i])===JSON.stringify(animal)){
-                return true;
-            }
-        }
-    }
-    return false;
-}
+        $("#save-button").text("Clear");
+        isSaved = true;
+    };
 
-function findTargetAnimal($this){
-    $animal = $this.parent();
-}
+    $("#animal-name").text(animal.name)
+    $("#animal-age").text(animal.age + " years old");
+    $("#animal-img").attr("src", animal.image);
+
+    $('#save-button').click(function(){
+        if(isSaved)
+        {
+            localStorage.removeItem("savedAnimal");
+            $("#save-button").css("display", "none");
+            $("#exp").text("Cleared an animal!");
+            $("#exp").css("display", "block");
+        }
+        else
+        {
+            localStorage.setItem("savedAnimal", JSON.stringify(animal));
+
+            $("#save-button").css("display", "none");
+            $("#exp").text("Saved an animal!");
+            $("#exp").css("display", "block");
+
+        }
+    });
+});
+
